4ce51c4b91ce78a73e485b6a45969ce342618857
Olga <olgaharceva946@gmail.com>
2022-11-03 19:53:42 +0300

    Решение задачи

--- Program/Program.cs
+++ Program/Program.cs
@@ -3,3 +3,58 @@
 // Входные данные можно получить путём ввода с клавиатуры, но лучше сделать автогенерацию данных. 
 // При решении не использовать "читерство".
 
+char[] GenerateRandom(int size)
+{
+    //генератор чисел и букв.
+    char[] textDigit = new char[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Char c = (char)new Random().Next('0', 'z');
+        if (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')))
+        {
+            textDigit[i] = c;
+        }
+    }
+    return textDigit;
+}
+
+
+
+// Сортируем массив
+char[] SortArray(char[] textDigit, int size)
+{
+    int count = 0;
+    foreach (char item in textDigit)
+    {
+        if (!char.IsDigit(item))
+        {
+            count++;
+        }
+    }
+    //Отделяем массив от чисел.
+    char[] newStrings = new char[count];
+    
+    int k = 0;
+    foreach (char item in textDigit)
+    {
+        if (!char.IsDigit(item))
+        {
+            newStrings[k] = item;
+            k++;
+        }
+    }
+    return newStrings;
+}
+
+
+
+int size = 30;  // - длина массива. 
+
+Console.WriteLine("Исходный массив: ");
+char[] array = GenerateRandom(size);
+Console.WriteLine(array);
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+Console.Write(SortArray(array, size));
+Console.WriteLine();
